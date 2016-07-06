f8a43af31fc02d75b249ae43c2eb1d9b4c15a017
20141105043 <670140530@qq.com>
2016-07-06 08:19:26 +0800

    Initial Commit

---
+++ D08swift_address list UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D08swift_address list UI
+//
+//  Created by 20141105043dj on 16/7/6.
+//  Copyright © 2016年 20141105043dj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ D08swift_address list UITests/D08swift_address_list_UITests.swift
@@ -0,0 +1,36 @@
+//
+//  D08swift_address_list_UITests.swift
+//  D08swift_address list UITests
+//
+//  Created by 20141105043dj on 16/7/6.
+//  Copyright © 2016年 20141105043dj. All rights reserved.
+//
+
+import XCTest
+@testable import D08swift_address_list_UI
+
+class D08swift_address_list_UITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
