2e70d34b9445f5e77c4b0076ac896a41dc3f142b
20141105043 <670140530@qq.com>
2016-07-08 08:13:39 +0800

    D08swift_address list
    图形界面完成并实现基本功能

---
+++ D08swift_address list UI/Header.h
@@ -0,0 +1,10 @@
+//
+//  Header.h
+//  D08swift_address list UI
+//
+//  Created by 20141105043dj on 16/7/6.
+//  Copyright © 2016年 20141105043dj. All rights reserved.
+//
+
+#import "sqlite3.h"
+#import <time.h>
--- D08swift_address list UI/ViewController.swift
+++ D08swift_address list UI/ViewController.swift
@@ -9,9 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var db:SQLiteDB!
+    
+    @IBOutlet weak var xingming: UITextField!
+   
+    @IBOutlet weak var shoujihao: UITextField!
 
+   
+    @IBAction func baocun(sender: UIButton) {
+        saveUser()
+        showAlert()
+    }
+    
+    func showAlert () {
+        var alert = UIAlertView(title:"提示",message:"添加联系人成功",delegate:self,cancelButtonTitle:"确定")
+        alert.alertViewStyle = UIAlertViewStyle.Default
+        alert.show()
+    }
+    func saveUser() {
+        let uname = self.xingming.text!
+        let mobile = self.shoujihao.text!
+        let sql = "insert into user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        let result = db.execute(sql)
+        print(result)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        db = SQLiteDB.sharedInstance()
+        
+        db.execute("create table if not exists t_user(uid integer primary key,unamevarchar(20),mobile varchar(20))")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
